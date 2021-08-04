d637c2d4de4e04035ea3ed1f5ef4adcf0c4a9eee
GoldenStar <68421054+GoldenStarq@users.noreply.github.com>
2021-08-04 18:30:23 +0430

    Create Remover.py

---
+++ Remover.py
@@ -0,0 +1,47 @@
+import requests, time
+import os
+os.system("title Massage Remover - `𝗦𝘁𝗮𝗿#4242")
+
+#Edit token
+TOKEN = 'TOKEN HERE'
+
+print("")
+print("░██████╗░░█████╗░██╗░░░░░██████╗░███████╗███╗░░██╗░██████╗████████╗░█████╗░██████╗░")
+print("██╔════╝░██╔══██╗██║░░░░░██╔══██╗██╔════╝████╗░██║██╔════╝╚══██╔══╝██╔══██╗██╔══██╗")
+print("██║░░██╗░██║░░██║██║░░░░░██║░░██║█████╗░░██╔██╗██║╚█████╗░░░░██║░░░███████║██████╔╝")
+print("██║░░╚██╗██║░░██║██║░░░░░██║░░██║██╔══╝░░██║╚████║░╚═══██╗░░░██║░░░██╔══██║██╔══██╗")
+print("╚██████╔╝╚█████╔╝███████╗██████╔╝███████╗██║░╚███║██████╔╝░░░██║░░░██║░░██║██║░░██║")
+print("")
+print("Creator: `𝗦𝘁𝗮𝗿#4242")
+print("-------------------")
+
+mode = input('[1] DM\n[2] Server\n')
+uid = input('User ID to purge: ')
+headers = {'Authorization':TOKEN}
+
+class Deleter:
+    def something(self, stuff):
+        messages = []
+        for block in stuff:
+            for message in block:
+                if message['author']['id'] == uid:
+                    messages.append('%s:%s' % (message['channel_id'], message['id']))
+        self.something_else(messages)
+    def something_else(self, messages):
+        print('Deleting %s messages.' % (len(messages)))
+        for message in messages:
+            channel = message.split(':')[0]
+            message_id = message.split(':')[-1]
+            r = requests.delete('https://discordapp.com/api/v6/channels/%s/messages/%s' % (channel, message_id), headers=headers)
+            time.sleep(0.2)
+obj = Deleter()
+if mode == '1':
+    channel_id = input('Channel ID: ')
+    while True:
+        r = requests.get('https://discordapp.com/api/v6/channels/%s/messages/search?author_id=%s' % (channel_id, uid), headers=headers)
+        obj.something(r.json()['messages'])
+else:
+    server = input('Server ID: ')
+    while True:
+        r = requests.get('https://discordapp.com/api/v6/guilds/%s/messages/search?author_id=%s&include_nsfw=true' % (server, uid), headers=headers)
+        obj.something(r.json()['messages'])
